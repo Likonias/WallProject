f88c1a8574a77558416205535452bd128bfe9591
Likonias <lisoan123@gmail.com>
2024-03-17 15:49:26 +0100

    small updates added some bindings

--- app/src/main/java/com/example/wallproject/View/DefaultScreen.kt
+++ app/src/main/java/com/example/wallproject/View/DefaultScreen.kt
@@ -3,18 +3,13 @@ package com.example.wallproject.View
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.example.wallproject.Controller.Game
-import com.example.wallproject.Controller.GoogleSignIn
 import com.example.wallproject.Model.GameSingleton
 import com.example.wallproject.R
 import com.example.wallproject.databinding.ActivityDefaultScreenBinding
-import com.google.android.gms.common.SignInButton
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -41,27 +36,28 @@ class DefaultScreen : AppCompatActivity() {
 
         game = GameSingleton.game
 
-        game.loadGame()
-
         binding.profileButton.setOnClickListener {
+            game.saveGame()
             startActivity(Intent(this, ProfileScreen::class.java))
         }
 
-        if(game.account.heroName == null){
-            showPopup("Enter hero name: ")
+        if(game.account.villageName == null){
+            showPopup("Enter village name: ")
             //todo finish popup logic
         }
 
-        startUpdatingSPSText()
+        startUpdatingData()
 
     }
 
-    private fun startUpdatingSPSText() {
+    private fun startUpdatingData() {
         //todo make it update the rest of the data, that needs to be updated the same way
         GlobalScope.launch(Dispatchers.Main) {
             while (true) {
-                // Update spsText
-                binding.spsText.text = game.wall.health.toString()
+
+                binding.spsText.text = game.tools.getSPS().toString()
+
+                binding.wallHealthText.text = game.wall.health.toString()
 
                 // Delay for 1 second
                 delay(1000)
@@ -82,7 +78,7 @@ class DefaultScreen : AppCompatActivity() {
 
         builder.setPositiveButton("OK") { dialog, which ->
             val userInput = editTextInput.text.toString()
-            game.account.heroName = userInput
+            game.account.villageName = userInput
             game.saveGame()
         }
 
