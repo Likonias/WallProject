9da53846efc2fe2b15b15516df04d0c4e872426b
Likonias <lisoan123@gmail.com>
2024-03-25 12:39:01 +0100

    updates views and coloring or elements

--- app/src/main/java/com/example/wallproject/View/ToolsScreen.kt
+++ app/src/main/java/com/example/wallproject/View/ToolsScreen.kt
@@ -2,10 +2,16 @@ package com.example.wallproject.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wallproject.Model.CustomAdapters.CustomAdapterToolsAdd
 import com.example.wallproject.Model.GameSingleton
 import com.example.wallproject.databinding.ActivityToolsScreenBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ToolsScreen : AppCompatActivity() {
 
@@ -25,5 +31,25 @@ class ToolsScreen : AppCompatActivity() {
         // Attach the adapter to the RecyclerView
         binding.recyclerView.adapter = customAdapterToolsAdd
 
+
+
+        startUpdatingData()
+
     }
+
+    private fun startUpdatingData() {
+
+        GlobalScope.launch(Dispatchers.Main) {
+            while (true) {
+
+                binding.stonesTextView.text = GameSingleton.game.wallet.getBalanceInt().toString()
+
+                // Delay for 0,1 second for smoother experience
+                delay(100)
+
+            }
+        }
+
+    }
+
 }
